e2485d9f4df5f55cef135fa0fba376e2410d54f8
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2022-12-26 10:09:47 +0200

    Time: 2103 ms (5.01%), Space: 17.9 MB (15.50%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    #define AboRadwan ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+const ll N = 1e6 + 50, NN = 105, mod = 1e9 + 7;
+#define all(v)        v.begin(), v.end()
+
+int jump(vector<int> &nums) {
+    AboRadwan;
+    int n = nums.size();
+    queue<int> q;
+    q.push(0);
+    vector<int> v(n, 1e9);
+    v[0]=0;
+    //[3,3,2,5,7,1,1,1,0,1]
+    int i = 1;
+    while (!q.empty()) {
+        if (q.front() == n - 1)return v[q.front()] ;
+        int x = nums[q.front()];
+        for (int j = 1; j <= x; ++j) {
+            if (j + q.front() >= n - 1)return v[q.front()] + 1;
+            if(v[j + q.front()]==1e9)q.push(j + q.front());
+            v[j + q.front()] = min(v[j + q.front()],v[q.front()] + 1);
+        }
+        
+        q.pop();
+    }
+    return v[n - 1];
+}
+};
