8f929944a183aa70535b19d4fcb44aa5978d2ccd
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-08 12:29:28 +0200

    Time: 31 ms (92.06%), Space: 13.4 MB (54.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -4,7 +4,7 @@ public:
 int maxPoints(vector<vector<int>> &points) {
     int n = points.size(), mx = 0;
     for (int i = 0; i < n; ++i) {
-        map<double, int> mp;
+        unordered_map<double, int> mp;
         for (int j = i + 1; j < n; ++j) {
             int y = points[i][1] - points[j][1],
                     x = points[i][0] - points[j][0];
