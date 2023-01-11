714c5b4023ca5eaeb2e0e527226727ec1908ecb0
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-11 11:45:02 +0200

    Time: 390 ms (44.63%), Space: 71.2 MB (22.82%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -4,12 +4,11 @@ public:
 static const ll N = 2e5, NN = 105, mod = 1e9 + 7;
 #define all(v)        v.begin(), v.end()
 
-void dfs(int i, vector<int> edges[], vector<int> &par, vector<bool> &vis) {
-    vis[i] = 1;
+void dfs(int i, vector<int> edges[], vector<int> &par) {
     for (auto item : edges[i]) {
-        if (!vis[item]) {
+        if (item!=par[i]) {
             par[item] = i;
-            dfs(item, edges, par, vis);
+            dfs(item, edges, par);
         }
     }
 }
@@ -20,20 +19,17 @@ int minTime(int n, vector<vector<int>> &edges, vector<bool> &hasApple) {
     par[0] = -1;
     vector<int> adj[n+5];
     for (auto i : edges)adj[i[0]].push_back(i[1]), adj[i[1]].push_back(i[0]);
-    dfs(0, adj, par, vis);
+    dfs(0, adj, par);
     ll cnt = 0;
     map<pair<int, int>, bool> mp;
-    for (int i=0;i<n;++i) {
+    for (int i = 0; i < n; ++i) {
         if (hasApple[i] == 0)continue;
-
         int x = i;
         while (par[x] != -1) {
             if (mp[{x, par[x]}] == 0)++cnt;
             mp[{x, par[x]}] = 1;
-            cout << x << " ";
             x = par[x];
         }
-        cout << endl;
     }
     return cnt * 2;
 }
