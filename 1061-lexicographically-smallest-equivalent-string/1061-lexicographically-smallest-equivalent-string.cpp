29160b0b54c8e61a308a94957b0a2448e46f209a
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-14 13:38:10 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (60.55%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         char ch[26];
-        for (char i='a'; i <='z'; i++)
-            ch[i-'a'] = i;
+        for (int i=0; i<26; i++)
+            ch[i] = 'a' + i;
 
         for (int i=0; i<s1.size(); i++) {
             char toReplace = max(ch[s1[i]-'a'], ch[s2[i]-'a']);
