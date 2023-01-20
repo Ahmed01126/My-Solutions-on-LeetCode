d912c14fc7678ba1dbb472b24ccc27f087e90f45
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-20 14:45:53 +0200

    Time: 220 ms (17.90%), Space: 96.7 MB (11.17%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(int i, int n, vector<int> &v, set<vector<int>> &s, vector<int> x = {}) {
+    if (i == n)return;
+    if (x.empty() || v[i] >= x.back()) {
+        x.push_back(v[i]);
+        // cout << v[i]<< " ";
+        backtrack(i + 1, n, v, s, x);
+        if(x.size()>1)s.insert(x);
+        x.pop_back();
+    }
+        // cout <<endl;
+    backtrack(i + 1, n, v, s, x);
+    
+}
+
+vector<vector<int>> findSubsequences(vector<int> &nums) {
+    set<vector<int>> s;
+    backtrack(0, nums.size(), nums, s);
+    vector<vector<int>>v;
+    for(auto i : s)v.push_back(i);
+    return v;
+}
+};
