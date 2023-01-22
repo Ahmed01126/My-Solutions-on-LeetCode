1974c7a6f5f3eed073cb8b56b9733bd6bfc19390
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-22 12:05:49 +0200

    Time: 195 ms (46.94%), Space: 136.6 MB (21.27%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string &s, int low, int high) {
+    while (low < high) {
+        if (s[low++] != s[high--]) return false;
+    }
+    return true;
+}
+
+void backtracking(vector<vector<string>> &v, int i, string &s, vector<string> vs={}) {
+    if(i>=s.size())
+        v.push_back(vs);
+    for (int j = i; j < s.size(); ++j) {
+        if (isPalindrome(s, i, j)) {
+            vs.push_back(s.substr(i, j - i + 1));
+            backtracking(v, j + 1, s, vs);
+            vs.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s) {
+    vector<vector<string>>v;
+    backtracking(v,0,s);
+    return v;
+}
+};
