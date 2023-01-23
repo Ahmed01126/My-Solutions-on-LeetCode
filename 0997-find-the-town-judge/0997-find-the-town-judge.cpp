358aff801d36a356bad34ef0379204c3391ea8f0
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-01-23 15:50:14 +0200

    Time: 425 ms (19.33%), Space: 60.8 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
     if(trust.empty() && n==1)return 1;
-    vector<int>v(n+1,n-1);
+    vector<int>v(n+1,0);
     int ans=1;
     for (int i = 0; i < trust.size(); ++i) {
         v[trust[i][1]]--;
-        v[trust[i][0]]=-1;
-        if(v[trust[i][1]]==0)ans= trust[i][1];
+        v[trust[i][0]]=1e5;
+        if(v[trust[i][1]]==-(n-1))ans= trust[i][1];
 
     }
-    if(v[ans]==0)return ans;
+    if(v[ans]==-(n-1))return ans;
     return -1;
 }
 };
