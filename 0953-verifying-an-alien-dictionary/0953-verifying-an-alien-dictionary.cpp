10fb87b2c30981158347d86c05bcc8e9163f8132
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-02-02 16:49:13 +0200

    Time: 0 ms (100.00%), Space: 9.5 MB (63.85%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string> &words, string order) {
+    map<char, int> mp;
+    int cnt = 0;
+    for (auto i : order)mp[i] = cnt++;
+    for (int i = 1; i < words.size(); ++i) {
+        if(words[i-1].find(words[i])!=-1 && words[i]!=words[i-1])return 0;
+        int x=0;
+         while (x<words[i].size() &&words[i][x]==words[i-1][x])++x;
+         if(x<words[i].size() && mp[words[i][x]] < mp[words[i-1][x]])return 0;
+         
+    }
+    return 1;
+}
+};
