e9665808a967b9d92c63b9bbbe1eb83d0c100154
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2023-05-05 12:06:17 +0300

    Time: 137 ms (64.76%), Space: 57.2 MB (95.00%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,bool>mp;
-        for(auto i: nums){
-            if(mp[i]==1)return 1;
-            mp[i]=1;
+        // map<int,bool>mp;
+        // for(auto i: nums){
+        //     if(mp[i]==1)return 1;
+        //     mp[i]=1;
+        // }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;++i){
+            if(nums[i]==nums[i+1])return 1;
         }
         return 0;
     }
