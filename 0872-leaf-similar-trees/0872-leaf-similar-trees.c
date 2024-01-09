0e64ef04f36e04d0fa6d41d1b5d6e12b76148ebe
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-09 23:04:03 +0200

    Time: 0 ms (100.00%), Space: 7.1 MB (39.68%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.c
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.c
@@ -10,7 +10,6 @@ int* ptr;
 void recursiveFun(struct TreeNode* root){
      if(root->left==NULL&& root->right==NULL){
         *ptr = root->val;
-         // printf("%d ",*ptr);
          ptr++;
         return;
     }
@@ -20,24 +19,13 @@ void recursiveFun(struct TreeNode* root){
 bool leafSimilar(struct TreeNode* root1, struct TreeNode* root2) {
     int lfs1[200], lfs2[200];
     for(int i=0;i<200;++i){
-        lfs1[i]=-1;
-        lfs2[i]=-1;
+        lfs1[i]=-1;lfs2[i]=-1;
     }
     ptr = lfs1;
     recursiveFun(root1);
     ptr = lfs2;
-    // printf("\n");
     recursiveFun(root2);
-   //  printf("\n");
-   // for(int i=0;i<200;++i){
-   //       printf("%d ",lfs1[i]);
-   //       printf("%d ",lfs2[i]);
-   //  }
-    // printf("\n");
-    
     for(int i=0;i<200;++i){
-         printf("%d ",lfs1[i]);
-         printf("%d ",lfs2[i]);
         if(lfs1[i]!=lfs2[i])return 0;
     }
     return 1;
