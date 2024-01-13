800cee3d99d6ffee0ced100c4b65c21641a3adf4
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-13 14:55:48 +0200

    Time: 12 ms (84.21%), Space: 9.3 MB (57.89%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.c
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.c
@@ -2,8 +2,6 @@ int minSteps(char* s, char* t) {
     int freq[27]={};
     for(int i=0;s[i]!='\0';++i){
         freq[s[i]-'a']++;
-    }
-    for(int i=0;t[i]!='\0';++i){
         freq[t[i]-'a']--;
     }
     int sum=0;
