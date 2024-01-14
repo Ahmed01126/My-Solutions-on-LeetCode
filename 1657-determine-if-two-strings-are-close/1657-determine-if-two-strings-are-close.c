edd7a94ab55de062f64effd9427a51e518696a94
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-14 21:23:35 +0200

    Time: 19 ms (67.31%), Space: 10.5 MB (40.38%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+int comp (const void * elem1, const void * elem2) 
+{
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
+
+bool closeStrings(char* s1, char* s2) {
+   int n = strlen(s1);
+   int nn = strlen(s2),cnt=0;
+    if(n!=nn)return 0;
+    int freq1[27]={}, freq2[27]={},v1[27]={},v2[27]={};
+    for(int i=0;s1[i]!='\0';++i){
+        freq1[s1[i]-'a']++;
+    }
+    for(int i=0;s2[i]!='\0';++i){
+        freq2[s2[i]-'a']++;
+    }
+    int idx1=0,idx2=0;
+    for(int i=0;i<27;++i){
+        if(freq1[i]!=0)v1[idx1++]=freq1[i];
+        if(freq2[i]!=0)v2[idx2++]=freq2[i];
+        if(freq1[i]!=0 && freq2[i]==0)return 0;
+        if(freq1[i]==0 && freq2[i]!=0)return 0;
+    }
+    qsort(v1,sizeof(v1)/sizeof(*v1), sizeof(*v1), comp);
+    qsort(v2,sizeof(v2)/sizeof(*v2), sizeof(*v2), comp);
+    for(int i=0;i<27;++i){
+        if(v1[i]!=v2[i])return 0;
+    }
+    return 1;
+}
