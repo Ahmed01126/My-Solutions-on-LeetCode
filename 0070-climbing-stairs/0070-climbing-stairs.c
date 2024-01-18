bd64e1b5ae4ddf4d6390546cc2d4cf9eb5ef1214
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-18 21:08:53 +0200

    Time: 3 ms (26.91%), Space: 6.3 MB (35.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,10 +1,10 @@
 int dp[46];
-int goDP(int n,int sum){
-    if(sum==n)return 1;
-    else if(sum>n)return 0;
-    if(dp[n-sum]!=0)return dp[n-sum];
-    return dp[n-sum] = goDP(n,sum+1)+goDP(n,sum+2);
+int goDP(int n){
+    if(0==n)return 1;
+    else if(0>n)return 0;
+    if(dp[n]!=0)return dp[n];
+    return dp[n] = goDP(n-1)+goDP(n-2);
 }
 int climbStairs(int n) {
-    return goDP(n,0);
+    return goDP(n);
 }
