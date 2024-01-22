4ce5e607289ddd64a3e617a91f5b330e54bfb924
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-22 16:41:56 +0200

    Time: 79 ms (12.49%), Space: 35.5 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -5,9 +5,7 @@ public:
         map<int,int>mp;
         for(int i=0;i<nums.size();++i){
             mp[nums[i]]++;
-        }
-        for(int i=1;i<=nums.size();++i){
-            if(mp[i]==2)v.push_back(i);
+            if(mp[nums[i]]==2)v.push_back(nums[i]);
         }
         for(int i=1;i<=nums.size();++i){
             if(mp[i]==0)v.push_back(i);
