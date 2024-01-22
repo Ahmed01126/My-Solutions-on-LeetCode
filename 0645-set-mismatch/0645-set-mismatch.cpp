4b03347a60e112afd11b3a3eea440b225ebac6c0
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-01-22 16:40:11 +0200

    Time: 95 ms (6.78%), Space: 35.3 MB (8.31%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>v;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();++i){
+            mp[nums[i]]++;
+        }
+        for(int i=1;i<=nums.size();++i){
+            if(mp[i]==2)v.push_back(i);
+        }
+        for(int i=1;i<=nums.size();++i){
+            if(mp[i]==0)v.push_back(i);
+        }
+        return v;
+    }
+};
