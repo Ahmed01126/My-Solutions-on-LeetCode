2f80a5e585792f6a97676d54e45273d6dc7ba112
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-06-06 00:07:44 +0300

    Time: 13 ms (27.96%), Space: 10.9 MB (90.77%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     vector<string> commonChars(vector<string> &words) {
     int n = words.size();
-    unordered_map<char, vector<int>> ump;
-    for (int i = 0; i < words.size(); ++i) {
-        for (int j = 0; j < 27; ++j)
-            ump[j + 'a'].push_back(0);
-    }
-    for (int i = 0; i < words.size(); ++i) {
-        for (auto j: words[i]) {
-            ump[j][i]++;
-        }
-    }
     vector<string> ans;
-    for (auto i: ump) {
-        int mn = 1e9;
-        for (auto j: i.second) {
-            mn = min(mn, j);
+    int freq[27][n + 1];
+    for (int i = 0; i < 27; ++i) {
+        for (int j = 0; j < n + 1; ++j) {
+            freq[i][j] = 0;
         }
-        
-        while (mn--){
+    }
+    for (int i = 0; i < 27; ++i) {
+        int mn = 1e9;
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < words[j].size(); ++k) {
+                if (i == words[j][k] - 'a')freq[i][j]++;
+            }
+            mn = min(mn, freq[i][j]);
+        }
+        while (mn > 0) {
+            mn--;
             string s = "";
-            s += i.first;
-            if(s!="")ans.push_back(s);
+            s += i + 'a';
+            ans.push_back(s);
         }
     }
     return ans;
