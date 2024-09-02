8f1fd422140627b88ccd99073fc7e177fff4f1d3
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-09-02 15:28:30 +0300

    Time: 1 ms (42.10%), Space: 41.2 MB (10.58%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+		byte cnt = 0;
+		while (n>0){
+            if((n & 1) == 1) {
+			    cnt++;
+            }
+			n >>= 1;
+		}
+		return cnt == 1;
+	}
+}
