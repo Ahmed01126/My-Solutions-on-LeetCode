aa43797c9f4ca8552c0a241775c05e11d700ffc1
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-09-02 15:36:33 +0300

    Time: 0 ms (100.00%), Space: 40.7 MB (65.51%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        byte cnt = 0;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                cnt++;
-            }
-            n >>= 1;
-        }
-        return cnt == 1;
+        if(n <= 0) return false;
+        n = (n & (n - 1));
+        return n == 0;
     }
 }
