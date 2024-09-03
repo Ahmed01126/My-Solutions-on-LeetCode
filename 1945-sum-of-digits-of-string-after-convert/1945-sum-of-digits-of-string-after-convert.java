cafe5d2ff2cfddd51005c2ba8457125d1c6720f6
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-09-03 09:30:21 +0300

    Time: 1 ms (88.60%), Space: 41.1 MB (98.21%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
@@ -1,10 +1,10 @@
 class Solution {
     public int getLucky(String s, int k) {
-        String newS ="";
+        StringBuilder newS = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            newS += (s.charAt(i) - 'a' + 1);
+            newS.append(s.charAt(i) - 'a' + 1);
         }
-        s = newS;
+        s = newS.toString();
         int sum = 0;
         while (k > 0) {
             sum = 0;
